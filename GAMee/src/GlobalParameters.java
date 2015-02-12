d66783a37313498d48cea4a90a4be106fd3104db
Shaun Carland <Shaun@Shauns-MacBook-Pro.local>
2015-02-12 13:48:49 -0500

    as'dfjlaskdf
    ,dfl’aksdf

--- GAMee/src/GlobalParameters.java
+++
@@ -1,7 +0,0 @@
-
-public class GlobalParameters {
-	public static int popSize = 1000; //should be factor of 100
-	public static int motifLength = 10; //how long each phrase is
-	public static int generations = 5; //number of Generations
-	
-}
